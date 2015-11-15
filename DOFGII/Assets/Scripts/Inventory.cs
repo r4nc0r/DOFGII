2e20dadd22be2a1a8d9bc65f5a68ddd85e22c138
Frenny <jennifer.buechl@googlemail.com>
2015-11-15 15:15:47 +0100

    Weapon Shop
    Weapon-Shop erstellt und Methoden eingebaut

---
+++ DOFGII/Assets/Scripts/Inventory.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour {
+
+    public  Weapon[] weapons = new Weapon[3];
+
+    public static Weapon aktuelleWeapon;
+
+    public Image SelectedImage;
+    public Text WeaponName;
+    public Text WeaponPreis;
+    private int position = 0;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //aktuelleWeapon = weapons[0];
+        //WeaponPreis.text = aktuelleWeapon.Preis;
+        //WeaponName.text = aktuelleWeapon.Name;
+    }
+
+    void Awake()
+    {
+        //aktuelleWeapon = weapons[0];
+        //WeaponPreis.text = aktuelleWeapon.Preis;
+        //WeaponName.text = aktuelleWeapon.Name;
+    }
+
+   
+    //Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void BuyWeapon()
+    {
+        aktuelleWeapon = weapons[position];
+        Application.LoadLevel("Main");
+    }
+
+    public void RightDirection()
+    {
+        if (position < weapons.Length - 1)
+        {
+            position++;
+            
+            WeaponName.text = weapons[position].Name;
+            WeaponPreis.text = weapons[position].Preis;
+            SelectedImage.sprite = weapons[position].sprite;
+        }
+
+    }
+
+    public void LeftDirection()
+    {
+        if (position > 0)
+        {
+            position--;
+            SelectedImage.sprite = weapons[position].sprite;
+            WeaponName.text = weapons[position].Name;
+            WeaponPreis.text = weapons[position].Preis;
+
+        }
+    }
+    public void BackToMiniGame()
+    {
+        Application.LoadLevel("Main");
+    }
+
+}
---
+++ DOFGII/Assets/Scripts/Weapon.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Weapon
+{
+    public string Name;
+    public string Preis;
+    public Sprite sprite;
+
+}
+
+   
+   
+    
