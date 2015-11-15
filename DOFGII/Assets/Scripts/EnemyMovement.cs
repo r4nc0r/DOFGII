49dfd3e02b7e763f47a0aa386e6fff289dbcd355
r4nc0r <samuel.seiz@gmail.com>
2015-11-15 14:03:05 +0100

    Enemy Prefab with Movement

---
+++ DOFGII/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+	NavMeshAgent nav;
+	GameObject player;
+	
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag ("Player");
+		nav = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		nav.SetDestination(player.transform.position);
+	}
+}
