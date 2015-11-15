7e5424511b7048a74ed9aae772eed35504ae9643
r4nc0r <samuel.seiz@gmail.com>
2015-11-15 16:35:20 +0100

    Added Enemy Spawn

--- DOFGII/Assets/Scripts/EnemyMovement.cs
+++ DOFGII/Assets/Scripts/EnemyMovement.cs
@@ -4,16 +4,40 @@ using System.Collections;
 public class EnemyMovement : MonoBehaviour {
 	NavMeshAgent nav;
 	GameObject player;
-	
+	GameObject enemy;
+	float distance;
+	public int shootDistance = 100;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindWithTag ("Player");
+		enemy = this.gameObject;
 		nav = GetComponent<NavMeshAgent> ();
+	}
+
+	void Shoot()
+	{
+		Debug.Log (this.gameObject+" :Shoot");
+//		NextShot = Time.time + fireRate;
+//		shotPosition = new Vector3(enemy.position.x,enemy.position.y+3,enemy.position.z);
+//		Instantiate(shot, shotPosition, enemy.rotation);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		double distance = Vector3.Distance(player.transform.position,enemy.transform.position);
+		if (distance > shootDistance) {
+			nav.SetDestination (player.transform.position);
+		}
+		else if (distance  <= shootDistance) {
+			nav.SetDestination (player.transform.position);
+			Shoot();
+
+		} 
+		else {
+
+		}
+
 		nav.SetDestination(player.transform.position);
 	}
 }
--- DOFGII/Assets/Scripts/GameController.cs
+++ DOFGII/Assets/Scripts/GameController.cs
@@ -2,14 +2,63 @@
 using System.Collections;
 
 public class GameController : MonoBehaviour {
+	public GameObject Enemy1;
+	public int Enemy1Count;
+	public int Enemy1OriginBegin;
+	public int Enemy1OriginWidth;
+
+	public GameObject Enemy2;
+	public int Enemy2Count;
+	public int Enemy2OriginBegin;
+	public int Enemy2OriginWidth;
+
+	public GameObject Enemy3;
+	public int Enemy3Count;
+	public int Enemy3OriginBegin;
+	public int Enemy3OriginWidth;
+
+	public GameObject Boundary;
+	public GameObject Player;
+	float distance;
+
+
+	/// <summary>
+	/// Awake this instance.
+	/// </summary>
+	void Awake()
+	{
+		distance = 50;//Vector3.Distance (Player.transform.position, Boundary.transform.position);
+		Enemy1Count = 15;
+	}
 
 	// Use this for initialization
 	void Start () {
-	
+		for (int i = 0; i < Enemy1Count; i++) {
+			SpawnEnemy(Enemy1, Enemy1Count, Enemy1OriginBegin,Enemy1OriginWidth);
+		}
+		for (int i = 0; i < Enemy2Count; i++) {
+			SpawnEnemy(Enemy2, Enemy2Count, Enemy2OriginBegin,Enemy2OriginWidth);
+		}
+		for (int i = 0; i < Enemy3Count; i++) {
+			SpawnEnemy(Enemy3, Enemy3Count, Enemy3OriginBegin,Enemy3OriginWidth);
+		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+//		if (GameObject.FindGameObjectsWithTag ("Enemy").Length < Enemy1Count) {
+//			SpawnEnemy (Enemy1, Enemy1Count,0, 90);
+//		}
+	}
+	Vector3 spawnPointTemp;
+	/// <summary>
+	/// Spawns the enemies.
+	/// </summary>
+	void SpawnEnemy(GameObject enemy, int enemyCount, int offset, int width)
+	{
+		int i = Random.Range(offset,offset+width);
+		spawnPointTemp.x = Mathf.Cos (Mathf.Deg2Rad*i) * (distance / 2);
+		spawnPointTemp.z = Mathf.Sin (Mathf.Deg2Rad*i) * (distance / 2);
+		Instantiate(enemy, spawnPointTemp , Quaternion.identity);
 	}
 }
